7d0a64e2e721e7f6f21b019d0e08ef4bddd3f742
Vladislav Kalinin <unkorunk@gmail.com>
2020-05-04 19:47:01 +1000

    Add get request feed

---
+++ app/src/main/java/ru/timelimit/healthtracking/GlobalStorage.kt
@@ -0,0 +1,7 @@
+package ru.timelimit.healthtracking
+
+class GlobalStorage {
+    companion object {
+        var token = "Emm3Eg03GZUkTCxF3iuzsnipFnc7IqcNQFk9T28frJ26PEGciPq6qV42GHFigSr0d57UFHVuw2WfhWBvmrsf6yjT5ipbzEVWNbWdlbEY2yw6mfEa7OxtjD4aZ3w7YMJl"
+    }
+}
--- app/src/main/java/ru/timelimit/healthtracking/ui/recommendation/RecommendationFragment.kt
+++ app/src/main/java/ru/timelimit/healthtracking/ui/recommendation/RecommendationFragment.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.textclassifier.TextSelection
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
+import ru.timelimit.healthtracking.GlobalStorage.Companion.token
 import ru.timelimit.healthtracking.R
 
 class RecommendationFragment : Fragment() {
     private val recommendationCards : MutableList<RecommendationCard> = arrayListOf()
     private lateinit var recommendationViewModel: RecommendationViewModel
+    private var requestQueue : RequestQueue? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,20 +31,40 @@ class RecommendationFragment : Fragment() {
         recommendationViewModel =
             ViewModelProviders.of(this).get(RecommendationViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_recommendation, container, false)
-        //todo getData from backend ASync recommended
-        val recommendationCard = RecommendationCard()
-        recommendationCard.author = "Ya"
-        recommendationCard.description = "Pizdato"
-        recommendationCard.publicationDate = "Nu ohyet mne uhe 2000 let"
-        recommendationCard.title = "Nu zdarova"
-        recommendationCard.recommendationImage = R.drawable.ic_home_black_24dp
-        recommendationCard.userImage = R.drawable.ic_dashboard_black_24dp
-        recommendationCards.add(recommendationCard)
-        recommendationCards.add(recommendationCard)
-        recommendationCards.add(recommendationCard)
-        val recyclerView = root.findViewById<RecyclerView>(R.id.recyclerview)
-        val adapter = DataAdapter(root.context, recommendationCards)
-        recyclerView.adapter = adapter
+
+        requestQueue = Volley.newRequestQueue(this.context)
+
+        val url = "http://104.248.59.99:8080/feed/get?token=$token"
+        val request = StringRequest(Request.Method.GET, url, Response.Listener {
+            val jsonObject = JSONObject(it)
+            if (jsonObject["status"] is Boolean && jsonObject["posts"] is JSONArray) {
+                val status = jsonObject["status"] as Boolean
+                val posts = jsonObject["posts"] as JSONArray
+                if (status) {
+                    for (i in 0 until posts.length()) {
+                        val post = posts.get(i) as JSONObject
+
+                        val recommendationCard = RecommendationCard()
+                        recommendationCard.author = post["author_name"] as String
+                        recommendationCard.description = post["description"] as String
+                        recommendationCard.publicationDate = post["publication_time"] as String
+                        recommendationCard.title = post["title"] as String
+                        recommendationCard.recommendationImage = R.drawable.ic_home_black_24dp
+                        recommendationCard.userImage = R.drawable.ic_dashboard_black_24dp
+                        recommendationCards.add(recommendationCard)
+
+                        val recyclerView = root.findViewById<RecyclerView>(R.id.recyclerview)
+                        val adapter = DataAdapter(root.context, recommendationCards)
+                        recyclerView.adapter = adapter
+                    }
+                }
+            }
+        }, Response.ErrorListener {
+            // TODO: error
+        })
+
+        requestQueue!!.add(request)
+
         return root
     }
 }
